42d9c2261f4823532e56a783d9747e5a3b291d38
schwaad <joseivoschwadea@gmail.com>
2025-03-23 09:55:34 -0300

    feat: Struct DataSet e funções primárias de manipulação do DataSet

--- src/data_set/mod.rs
+++ src/data_set/mod.rs
@@ -1,1 +1,26 @@
 pub mod table;
+
+use crate::table::*;
+use std::fs::*;
+
+pub struct DataSet{
+    tables : Vec<Table>
+}
+
+pub fn create_empty_data_set() -> DataSet {
+    DataSet {
+        tables: Vec::<Table>::new(), 
+    }
+}
+
+pub fn add_table_to_data_set(mut currentDataSet : DataSet, newTable : Table) -> DataSet {
+    currentDataSet.tables.push(newTable);
+    currentDataSet
+}
+
+pub fn print_data_set_tables(currentDataSet : DataSet ){
+    for table in currentDataSet.tables {
+        print_table(table);
+    }
+}
+
