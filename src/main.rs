764fdf2bdc7b0bc32f9c0b74abcaa31ba37117d7
schwaad <joseivoschwadea@gmail.com>
2025-03-23 09:58:02 -0300

    test: adição de testes simples das funções de manipulação do data set na função main

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
-mod data_set;
-
-fn clear_terminal() {
-    print!("\x1B[2J\x1B[1;1H");
-}
-
-fn menu() {
-   clear_terminal();
-    println!("Welcome to my simplified SGBD written in Rust!\n
-Choose one of the following options:\n 
-1 - Create a new database\n
-2 - Access an existing database\n
-0 - Quit\n");
-}
+pub mod data_set;
+pub mod tui;
+use crate::tui::*;
+use crate::data_set::*;
+use crate::data_set::table::*;
+use crate::data_set::table::column::*;
+use crate::data_set::table::column::ColumnType::Int;
+use crate::data_set::table::row::*;
 
 
 fn main() {
-    use crate::data_set::table::test;
+    clear_terminal();
     menu();
-    test();
+    /*
+    let mut newDataSet = create_empty_data_set();
+    let mut newTable = create_empty_table();
+    newTable = add_column(newTable, create_column(Int));
+    newTable = add_column(newTable, create_column(Int));
+    newTable = add_row(newTable, create_row());
+    newTable.rows[0].cells.push("1".to_string());
+    newTable.rows[0].cells.push("2".to_string());
+    newTable = add_row(newTable, create_row());
+    newTable.rows[1].cells.push("3".to_string());
+    newTable.rows[1].cells.push("4".to_string());
+    println!("minha tabela: ");
+    print_table(newTable);
+    */
 }
